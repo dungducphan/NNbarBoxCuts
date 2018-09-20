29d85aa3b2810a70342c3faa3f5db74b690cdb4f
Dung Phan <brianp.dung@utexas.edu>
2018-09-19 23:23:16 -0500

    2nd ci.

--- TreeAccess.h
+++ TreeAccess.h
@@ -5,6 +5,7 @@
 #include <TChain.h>
 #include <TFile.h>
 #include <TH2.h>
+#include <TH1.h>
 #include <TStyle.h>
 #include <TCanvas.h>
 
@@ -15,108 +16,132 @@
 
 class TreeAccess {
 public :
-  TreeAccess(std::string, TTree *tree = 0);
-  virtual ~TreeAccess();
+    explicit TreeAccess(std::string, TTree *tree = nullptr);
+    virtual ~TreeAccess();
 
-  virtual Int_t Cut(Long64_t entry);
-  virtual Int_t GetEntry(Long64_t entry);
-  virtual Long64_t LoadTree(Long64_t entry);
-  virtual void Init(TTree *tree);
-  virtual void Loop();
-  virtual Bool_t Notify();
-  virtual void Show(Long64_t entry = -1);
-  virtual void Terminate(std::string);
-  virtual TH1D* GetTH1DByName(std::string);
-  virtual void Normalize(TH1D*, double normFactor = 0.);
-  virtual void SetSignal();
-  virtual void SetEnergyCut();
-  virtual void SetEnergyPerHitYViewCut();
-  virtual void SetEnergyPerHitXViewCut();
-  virtual void SetAverageYPositionCut();
+    virtual Int_t Cut(Long64_t entry);
+    virtual Int_t GetEntry(Long64_t entry);
+    virtual Long64_t LoadTree(Long64_t entry);
+    virtual void Init(TTree *tree);
+    virtual void Loop();
+    virtual Bool_t Notify();
+    void Show(Long64_t entry = -1);
+    virtual void Terminate(std::string);
+    virtual void Terminate2D(std::string);
+    virtual TH1D *GetTH1DByName(std::string);
+    virtual TH2D *GetTH2DByName(std::string);
+    void Normalize(TH1D * hist, double normFactor = 0.);
+    virtual void SetSignal();
+
+    virtual void SetEnergyCut();
+    virtual void SetEnergyPerHitYViewCut();
+    virtual void SetEnergyPerHitXViewCut();
+    virtual void SetAverageYPositionCut();
+    virtual void SetTotHitCountXViewCut();
+    virtual void SetTotHitCountYViewCut();
+    virtual void SetAreaRatioXViewCut();
+    virtual void SetAreaRatioYViewCut();
+    virtual void SetAvgEPerHitCompositeCut();
 
 private:
-  virtual bool PassVisECut();
-  virtual bool PassAvgEPerHitYViewCut();
-  virtual bool PassAvgEPerHitXViewCut();
-  virtual bool PassAvgYPositionCut();
+    virtual bool PassVisECut();
+    virtual bool PassAvgEPerHitYViewCut();
+    virtual bool PassAvgEPerHitXViewCut();
+    virtual bool PassAvgYPositionCut();
+    virtual bool PassTotHitCountXViewCut();
+    virtual bool PassTotHitCountYViewCut();
+    virtual bool PassAreaRatioXViewCut();
+    virtual bool PassAreaRatioYViewCut();
+    virtual bool PassAvgEPerHitCompositeCut();
 
-  bool fVisECut;
-  bool fAvgEPerHitYViewCut;
-  bool fAvgEPerHitXViewCut;
-  bool fAvgYPositionCut;
+    bool fVisECut;
+    bool fAvgEPerHitYViewCut;
+    bool fAvgEPerHitXViewCut;
+    bool fAvgYPositionCut;
+    bool fTotHitCountXViewCut;
+    bool fTotHitCountYViewCut;
+    bool fAreaRatioXViewCut;
+    bool fAreaRatioYViewCut;
+    bool fAvgEPerHitCompositeCut;
 
-  bool fIsCosmic;
+    bool fIsCosmic;
 
-  TTree *fChain;
-  Int_t fCurrent;
+    TTree *fChain;
+    Int_t fCurrent;
 
-  Double_t TotalVisibleEnergy;
-  Double_t TotalHitCountInXView;
-  Double_t TotalHitCountInYView;
-  Double_t HitCountXYDifference;
-  Double_t TemporalClusterExpand;
-  Double_t AverageEnergyPerHitXView;
-  Double_t AverageEnergyPerHitYView;
-  Double_t CorrelationTCellXView;
-  Double_t CorrelationTPlaneXView;
-  Double_t CorrelationTCellYView;
-  Double_t CorrelationTPlaneYView;
-  Double_t ConvexHullAreaXV;
-  Double_t ConvexHullAreaYV;
-  Double_t MinimalEnclosingCircleAreaXV;
-  Double_t MinimalEnclosingCircleAreaYV;
-  Double_t AverageYposition;
+    Double_t TotalVisibleEnergy;
+    Double_t TotalHitCountInXView;
+    Double_t TotalHitCountInYView;
+    Double_t HitCountXYDifference;
+    Double_t TemporalClusterExpand;
+    Double_t AverageEnergyPerHitXView;
+    Double_t AverageEnergyPerHitYView;
+    Double_t CorrelationTCellXView;
+    Double_t CorrelationTPlaneXView;
+    Double_t CorrelationTCellYView;
+    Double_t CorrelationTPlaneYView;
+    Double_t ConvexHullAreaXV;
+    Double_t ConvexHullAreaYV;
+    Double_t MinimalEnclosingCircleAreaXV;
+    Double_t MinimalEnclosingCircleAreaYV;
+    Double_t AverageYposition;
 
-  TBranch *b_TotalVisibleEnergy;
-  TBranch *b_TotalHitCountInXView;
-  TBranch *b_TotalHitCountInYView;
-  TBranch *b_HitCountXYDifference;
-  TBranch *b_TemporalClusterExpand;
-  TBranch *b_AverageEnergyPerHitXView;
-  TBranch *b_AverageEnergyPerHitYView;
-  TBranch *b_CorrelationTCellXView;
-  TBranch *b_CorrelationTPlaneXView;
-  TBranch *b_CorrelationTCellYView;
-  TBranch *b_CorrelationTPlaneYView;
-  TBranch *b_ConvexHullAreaXV;
-  TBranch *b_ConvexHullAreaYV;
-  TBranch *b_MinimalEnclosingCircleAreaXV;
-  TBranch *b_MinimalEnclosingCircleAreaYV;
-  TBranch *b_AverageYposition;
+    TBranch *b_TotalVisibleEnergy;
+    TBranch *b_TotalHitCountInXView;
+    TBranch *b_TotalHitCountInYView;
+    TBranch *b_HitCountXYDifference;
+    TBranch *b_TemporalClusterExpand;
+    TBranch *b_AverageEnergyPerHitXView;
+    TBranch *b_AverageEnergyPerHitYView;
+    TBranch *b_CorrelationTCellXView;
+    TBranch *b_CorrelationTPlaneXView;
+    TBranch *b_CorrelationTCellYView;
+    TBranch *b_CorrelationTPlaneYView;
+    TBranch *b_ConvexHullAreaXV;
+    TBranch *b_ConvexHullAreaYV;
+    TBranch *b_MinimalEnclosingCircleAreaXV;
+    TBranch *b_MinimalEnclosingCircleAreaYV;
+    TBranch *b_AverageYposition;
 
-  TH1D* h_TotalVisibleEnergy;
-  TH1D* h_TotalHitCountInXView;
-  TH1D* h_TotalHitCountInYView;
-  TH1D* h_AverageEnergyPerHitXView;
-  TH1D* h_AverageEnergyPerHitYView;
-  TH1D* h_HitCountXYDifference;
-  TH1D* h_AreaRatioXV;
-  TH1D* h_AreaRatioYV;
-  TH1D* h_AverageYPosition;
+    // TH1
+    TH1D *h_TotalVisibleEnergy;
+    TH1D *h_TotalHitCountInXView;
+    TH1D *h_TotalHitCountInYView;
+    TH1D *h_AverageEnergyPerHitXView;
+    TH1D *h_AverageEnergyPerHitYView;
+    TH1D *h_HitCountXYDifference;
+    TH1D *h_AreaRatioXV;
+    TH1D *h_AreaRatioYV;
+    TH1D *h_AverageYPosition;
 
-  double nf_TotalVisibleEnergy_cosmic;
-  double nf_TotalHitCountInXView_cosmic;
-  double nf_TotalHitCountInYView_cosmic;
-  double nf_AverageEnergyPerHitXView_cosmic;
-  double nf_AverageEnergyPerHitYView_cosmic;
-  double nf_HitCountXYDifference_cosmic;
-  double nf_AreaRatioXV_cosmic;
-  double nf_AreaRatioYV_cosmic;
-  double nf_AverageYPosition_cosmic;
+    // TH2
+    TH2D *h2D_AverageEnergyPerHit;
+    TH2D *h2D_AreaRatio;
 
-  double nf_TotalVisibleEnergy_signal;
-  double nf_TotalHitCountInXView_signal;
-  double nf_TotalHitCountInYView_signal;
-  double nf_AverageEnergyPerHitXView_signal;
-  double nf_AverageEnergyPerHitYView_signal;
-  double nf_HitCountXYDifference_signal;
-  double nf_AreaRatioXV_signal;
-  double nf_AreaRatioYV_signal;
-  double nf_AverageYPosition_signal;
+    double nf_TotalVisibleEnergy_cosmic;
+    double nf_TotalHitCountInXView_cosmic;
+    double nf_TotalHitCountInYView_cosmic;
+    double nf_AverageEnergyPerHitXView_cosmic;
+    double nf_AverageEnergyPerHitYView_cosmic;
+    double nf_HitCountXYDifference_cosmic;
+    double nf_AreaRatioXV_cosmic;
+    double nf_AreaRatioYV_cosmic;
+    double nf_AverageYPosition_cosmic;
 
-  std::map<std::string, double> NFMaps_Cosmic;
-  std::map<std::string, double> NFMaps_Signal;
-  std::map<std::string, TH1D*> TH1DMaps;
+    double nf_TotalVisibleEnergy_signal;
+    double nf_TotalHitCountInXView_signal;
+    double nf_TotalHitCountInYView_signal;
+    double nf_AverageEnergyPerHitXView_signal;
+    double nf_AverageEnergyPerHitYView_signal;
+    double nf_HitCountXYDifference_signal;
+    double nf_AreaRatioXV_signal;
+    double nf_AreaRatioYV_signal;
+    double nf_AverageYPosition_signal;
+
+    std::map<std::string, double> NFMaps_Cosmic;
+    std::map<std::string, double> NFMaps_Signal;
+    std::map<std::string, TH1D *> TH1DMaps;
+    std::map<std::string, TH2D *> TH2DMaps;
 };
 
 #endif
--- main.cpp
+++ main.cpp
@@ -5,79 +5,153 @@
 #include <TLegend.h>
 
 int main() {
-  GeneralStyle();
+    GeneralStyle();
 
-  // Loading the data samples
-  TreeAccess* cosmicTree = new TreeAccess("/Users/dphan/nova.nnbar.prongAnalyzer/OfflinePlotting/NNbar_Analysis_Hist.Cosmic.root");
-  TreeAccess* signalTree = new TreeAccess("/Users/dphan/nova.nnbar.prongAnalyzer/OfflinePlotting/NNbar_Analysis_Hist.Signal.root");
+    TreeAccess *cosmicTree = new TreeAccess("/home/dphan/nova.nnbar/composeCuts/NNbarBoxCuts/NNbar_Analysis_Hist.Cosmic.root");
+    TreeAccess *signalTree = new TreeAccess("/home/dphan/nova.nnbar/composeCuts/NNbarBoxCuts/NNbar_Analysis_Hist.Signal.root");
+    signalTree->SetSignal();
 
-  // Setting up the sample objects (always do this before Looping)
-  signalTree->SetSignal();
+//    // Setting cuts
+//    std::string cutLevel = "";
+//
+//    cosmicTree->SetAverageYPositionCut();
+//    signalTree->SetAverageYPositionCut();
+//    cutLevel += ".AvgYPos";
+//
+//    cosmicTree->SetEnergyCut();
+//    signalTree->SetEnergyCut();
+//    cutLevel += ".VisE";
 
-  // Setting cuts
-  cosmicTree->SetEnergyCut();
-  signalTree->SetEnergyCut();
-  cosmicTree->SetEnergyPerHitYViewCut();
-  signalTree->SetEnergyPerHitYViewCut();
-  cosmicTree->SetEnergyPerHitXViewCut();
-  signalTree->SetEnergyPerHitXViewCut();
-  cosmicTree->SetAverageYPositionCut();
-  signalTree->SetAverageYPositionCut();
+//    cosmicTree->SetAvgEPerHitCompositeCut();
+//    signalTree->SetAvgEPerHitCompositeCut();
+//    cutLevel += ".AvgEHitComposite";
+
+/*
+    cosmicTree->SetEnergyPerHitYViewCut();
+    signalTree->SetEnergyPerHitYViewCut();
+    cutLevel += ".AvgEHitY";
+
+    cosmicTree->SetEnergyPerHitXViewCut();
+    signalTree->SetEnergyPerHitXViewCut();
+    cutLevel += ".AvgEHitX";
+*/
+
+//    cosmicTree->SetTotHitCountXViewCut();
+//    signalTree->SetTotHitCountXViewCut();
+//    cutLevel += ".TotHitCountX";
+//
+//    cosmicTree->SetTotHitCountYViewCut();
+//    signalTree->SetTotHitCountYViewCut();
+//    cutLevel += ".TotHitCountY";
+//
+//    cosmicTree->SetAreaRatioXViewCut();
+//    signalTree->SetAreaRatioXViewCut();
+//    cutLevel += ".AreaRatioX";
+//
+//    cosmicTree->SetAreaRatioYViewCut();
+//    signalTree->SetAreaRatioYViewCut();
+//    cutLevel += ".AreaRatioY";
+
+    // Looping
+//    cosmicTree->Loop();
+//    signalTree->Loop();
+
+/*
+    //------------------------------------------------------------------------------------------------------------------
+    std::vector<std::string> VarList;
+    VarList.clear();
+    VarList.push_back("TotalVisibleEnergy");
+    VarList.push_back("TotalHitCountInXView");
+    VarList.push_back("TotalHitCountInYView");
+    VarList.push_back("AverageEnergyPerHitXView");
+    VarList.push_back("AverageEnergyPerHitYView");
+    VarList.push_back("HitCountXYDifference");
+    VarList.push_back("AreaRatioXV");
+    VarList.push_back("AreaRatioYV");
+    VarList.push_back("AverageYPosition");
+
+    for (unsigned int i = 0; i < VarList.size(); i++) {
+        // Variables in question
+        std::string VariableName = VarList.at(i);
+        std::string AlternativeName_Cosmic = VariableName + "Cosmic";
+        std::string AlternativeName_Signal = VariableName + "Signal";
+
+        // Event Loop
+
+        TH1D *hist2Plot_cosmic = (TH1D *) cosmicTree->GetTH1DByName(VariableName)->Clone(
+                AlternativeName_Cosmic.c_str());
+        cosmicTree->Terminate(VariableName.c_str());
+
+        TH1D *hist2Plot_signal = (TH1D *) signalTree->GetTH1DByName(VariableName)->Clone(
+                AlternativeName_Signal.c_str());
+        signalTree->Terminate(VariableName.c_str());
+
+        // Plotting
+        auto c = new TCanvas("c", "c", 200, 200);
+        StyleLinearLinear(c);
+        hist2Plot_cosmic->GetYaxis()->SetRangeUser(0, 1.5);
+        hist2Plot_cosmic->GetXaxis()->CenterTitle();
+        hist2Plot_cosmic->GetYaxis()->CenterTitle();
+
+        hist2Plot_cosmic->SetLineWidth(3);
+        hist2Plot_signal->SetLineWidth(3);
+        hist2Plot_signal->SetLineColor(kRed);
+
+        hist2Plot_cosmic->Draw("HIST");
+        hist2Plot_signal->Draw("HIST SAME");
+
+        TLegend *leg = new TLegend(0.55, 0.7, 0.8, 0.85);
+        leg->AddEntry(hist2Plot_cosmic, "Cosmic", "l");
+        leg->AddEntry(hist2Plot_signal, "Signal", "l");
+        leg->Draw();
+
+        std::string plotFolder = "/home/dphan/nova.nnbar/composeCuts/NNbarBoxCuts/Plots";
+        c->SetFixedAspectRatio();
+        c->SaveAs(Form("%s/%s%s.pdf", plotFolder.c_str(), VariableName.c_str(), cutLevel.c_str()));
+        delete c;
+    }
+*/
 
 
-  // Looping
-  cosmicTree->Loop();
-  signalTree->Loop();
+    //------------------------------------------------------------------------------------------------------------------
+//    std::vector<std::string> VarList2D;
+//    VarList2D.clear();
+//    VarList2D.push_back("AverageEnergyPerHit");
+//    VarList2D.push_back("AreaRatio");
+//    for (unsigned int i = 0; i < VarList2D.size(); i++) {
+//        // Variables in question
+//        std::string VariableName = VarList2D.at(i);
+//        std::string AlternativeName_Cosmic = VariableName + "Cosmic";
+//        std::string AlternativeName_Signal = VariableName + "Signal";
 
-  std::vector<std::string> VarList; VarList.clear();
-  VarList.push_back("TotalVisibleEnergy");
-  VarList.push_back("TotalHitCountInXView");
-  VarList.push_back("TotalHitCountInYView");
-  VarList.push_back("AverageEnergyPerHitXView");
-  VarList.push_back("AverageEnergyPerHitYView");
-  VarList.push_back("HitCountXYDifference");
-  VarList.push_back("AreaRatioXV");
-  VarList.push_back("AreaRatioYV");
-  VarList.push_back("AverageYPosition");
+//        // Event Loop
+//
+//        TH2D *hist2Plot_cosmic = (TH2D*) cosmicTree->GetTH2DByName(VariableName)->Clone(AlternativeName_Cosmic.c_str());
+//        hist2Plot_cosmic->SetTitle("Cosmic");
+//        hist2Plot_cosmic->GetXaxis()->CenterTitle();
+//        hist2Plot_cosmic->GetYaxis()->CenterTitle();
+//        cosmicTree->Terminate2D(VariableName.c_str());
+//
+//        TH2D *hist2Plot_signal = (TH2D*) signalTree->GetTH2DByName(VariableName)->Clone(AlternativeName_Signal.c_str());
+//        hist2Plot_signal->SetTitle("Signal");
+//        hist2Plot_signal->GetXaxis()->CenterTitle();
+//        hist2Plot_signal->GetYaxis()->CenterTitle();
+//        signalTree->Terminate2D(VariableName.c_str());
+//
+//        // Plotting
+//        auto c = new TCanvas("c", "c", 200, 400);
+//        c->Divide(2, 1);
+//
+//        c->cd(1);
+//        hist2Plot_cosmic->Draw("COLZ");
+//        c->cd(2);
+//        hist2Plot_signal->Draw("COLZ");
+//
+//        std::string plotFolder = "/home/dphan/nova.nnbar/composeCuts/NNbarBoxCuts/Plots";
+//        c->SetFixedAspectRatio();
+//        c->SaveAs(Form("%s/%s%s.pdf", plotFolder.c_str(), VariableName.c_str(), cutLevel.c_str()));
+//        delete c;
+//    }
 
-  for (unsigned int i = 0; i < VarList.size(); i++) {
-    // Variables in question
-    std::string VariableName = VarList.at(i);
-    std::string AlternativeName_Cosmic = VariableName + "Cosmic";
-    std::string AlternativeName_Signal = VariableName + "Signal";
-
-    // Event Loop
-
-    TH1D* hist2Plot_cosmic = (TH1D*) cosmicTree->GetTH1DByName(VariableName)->Clone(AlternativeName_Cosmic.c_str());
-    cosmicTree->Terminate(VariableName.c_str());
-
-    TH1D* hist2Plot_signal = (TH1D*) signalTree->GetTH1DByName(VariableName)->Clone(AlternativeName_Signal.c_str());
-    signalTree->Terminate(VariableName.c_str());
-
-    // Plotting
-    auto c = new TCanvas("c", "c", 200, 200);
-    StyleLinearLinear(c);
-    hist2Plot_cosmic->GetYaxis()->SetRangeUser(0, 1.5);
-    hist2Plot_cosmic->GetXaxis()->CenterTitle();
-    hist2Plot_cosmic->GetYaxis()->CenterTitle();
-
-    hist2Plot_cosmic->SetLineWidth(3);
-    hist2Plot_signal->SetLineWidth(3);
-    hist2Plot_signal->SetLineColor(kRed);
-
-    hist2Plot_cosmic->Draw("HIST");
-    hist2Plot_signal->Draw("HIST SAME");
-
-    TLegend* leg = new TLegend(0.55, 0.7, 0.8, 0.85);
-    leg->AddEntry(hist2Plot_cosmic, "Cosmic", "l");
-    leg->AddEntry(hist2Plot_signal, "Signal", "l");
-    leg->Draw();
-
-    std::string plotFolder = "/Users/dphan/nova.nnbar.prongAnalyzer/OfflinePlotting/Plots";
-    c->SetFixedAspectRatio();
-    c->SaveAs(Form("%s/%s.AllCut.pdf", plotFolder.c_str(), VariableName.c_str()));
-    delete c;
-  }
-
-  return 0;
+    return 0;
 }
